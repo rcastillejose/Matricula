b0c8a8a2f0a22c66aa3536f999870c870614ed6d
Raul <Raul@192.168.1.3>
2019-06-01 21:41:57 +0200

    a

--- src/controlador/ControladorReservas.java
+++ src/controlador/ControladorReservas.java
@@ -1,5 +1,79 @@
 package controlador;
 
-public class ControladorReservas {
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+import baseDeDatos.Modelo;
+import modelo.Alumno;
+import vista.JFAdministrador;
+import vista.JFLogin;
+import vista.JFReservar;
+
+public class ControladorReservas implements WindowListener {
+	private JFReservar jfre ;
+	private Modelo modelo;
+	private JFLogin vista;
+	private Alumno a;
+	
+	public ControladorReservas(JFLogin vista, JFReservar jfre, Modelo modelo, Alumno a) {
+		this.jfre = jfre;
+		this.modelo = modelo;
+		this.vista = vista;
+		this.a=a;
+		inicializar();
+	}
+	private void inicializar() {
+
+		jfre.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		jfre.addWindowListener(this);
+
+		// Anadir las acciones a los botones del formulario padre
+
+		// Ponemos a escuchar las acciones del usuario
+
+	}
+
+	public void go() {
+		jfre.setVisible(true);
+	}
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("Cerrando");
+		vista.setVisible(true);
+		jfre.dispose();
+	}
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/vista/JFReservar.java
+++ src/vista/JFReservar.java
@@ -8,10 +8,26 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import modelo.Alumno;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import com.github.lgooddatepicker.components.CalendarPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.border.TitledBorder;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 
 public class JFReservar extends JFrame {
 
-	private JPanel contentPane;
+	public JPanel contentPane;
+	public JTable tReservas;
+	public CalendarPanel cPDiaReserva;
+	public JButton btnReservar;
+	private JLabel lblCurso;
+	public JComboBox cBCurso;
 	public Alumno a;
 
 	/**
@@ -36,11 +52,74 @@ public class JFReservar extends JFrame {
 	public JFReservar(Alumno a) {
 		this.a=a;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 927, 672);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		cPDiaReserva = new CalendarPanel();
+		cPDiaReserva.setBorder(new TitledBorder(null, "Dia de Reserva", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		JLabel lblHazTuReserva = new JLabel("HAZ TU RESERVA");
+		lblHazTuReserva.setFont(new Font("Dialog", Font.BOLD, 21));
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Reservas disponibles", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		btnReservar = new JButton("Reservar");
+		
+		lblCurso = new JLabel("Curso");
+		
+		cBCurso = new JComboBox();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(369, Short.MAX_VALUE)
+					.addComponent(lblHazTuReserva)
+					.addGap(352))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(814, Short.MAX_VALUE)
+					.addComponent(btnReservar)
+					.addContainerGap())
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(64)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblCurso)
+							.addGap(18)
+							.addComponent(cBCurso, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(cPDiaReserva, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 101, Short.MAX_VALUE)
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 354, GroupLayout.PREFERRED_SIZE)
+							.addGap(91))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblHazTuReserva)
+					.addGap(58)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCurso)
+						.addComponent(cBCurso, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(cPDiaReserva, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 343, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 111, Short.MAX_VALUE)
+					.addComponent(btnReservar)
+					.addContainerGap())
+		);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		tReservas = new JTable();
+		scrollPane.setViewportView(tReservas);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
